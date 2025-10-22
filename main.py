0e3f464e31325702fca8f6ffe8deff1f41b94ce7
Dimitri Skliros <skliros@quantsuite.io>
2025-10-22 16:49:06 +0300

    Check that streamlit link, https://scrubber-analytics.streamlit.app, works

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #main.py
 import streamlit as st
+import pandas as pd
+from numpy.random import default_rng as rng
 
+df = pd.DataFrame(
+    rng(0).standard_normal((50,20)), columns=([f"col {d}" for d in range(20)])
+)
 
+st.dataframe(df)
